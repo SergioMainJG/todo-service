b5fed92862a1cdcd95c5020761de42dfd631431e
Sergio Arce <arce.roldan.sergio@gmail.com>
2024-12-10 15:30:55 -0600

    add: todo.repository.impl.ts

---
+++ src/todo-service/infrastructure/repositories/todo.repository.impl.ts
@@ -0,0 +1,22 @@
+import { TodoEntity, TodoCharacteristic, TodoRepository, TodoDatasource } from '../..';
+
+export class TodoRepositoryImpl implements TodoRepository {
+    constructor(
+        private readonly todoDatasource: TodoDatasource
+    ) {}
+    async addTodo(todo: TodoEntity): Promise<void> {
+        this.todoDatasource.addTodo(todo);
+    }
+    async updateTodo(id: string): Promise<void> {
+        this.todoDatasource.updateTodo(id);
+    }
+    async deleteTodo(id: string): Promise<void> {
+        this.todoDatasource.deleteTodo(id);
+    }
+    async getTodos(): Promise<TodoEntity[]> {
+        return this.todoDatasource.getTodos();
+    }
+    async getStrategy(characteristic: TodoCharacteristic): Promise<TodoEntity> {
+        return this.todoDatasource.getStrategy(characteristic);
+    }
+}
