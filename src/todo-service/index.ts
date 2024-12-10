d0b1a8fe4d4b4bc33b8192e2835269746c2e8455
Sergio Arce <arce.roldan.sergio@gmail.com>
2024-12-10 16:07:23 -0600

    add: todo-service/index

---
+++ src/todo-service/index.ts
@@ -0,0 +1,7 @@
+export * from './d/todo.d';
+export * from './domain/datasource/todo.datasource';
+export * from './domain/entities/todo.entity';
+export * from './domain/pattern/todo.builder';
+export * from './domain/repository/todo.repository';
+export * from './infrastructure/datasources/LocalStorage.datasource';
+export * from './infrastructure/repositories/todo.repository.impl';
