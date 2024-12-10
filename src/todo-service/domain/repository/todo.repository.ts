33e0a0fdcb6fdbe6e8d670d71ddfc34cb1d5b15a
Sergio Arce <arce.roldan.sergio@gmail.com>
2024-12-10 15:30:09 -0600

    update: todo.repository.ts & todo.datasource.ts: createTodo => addTodo;

--- src/todo-service/domain/repository/todo.repository.ts
+++ src/todo-service/domain/repository/todo.repository.ts
@@ -2,9 +2,9 @@ import { TodoEntity } from "../..";
 import { TodoCharacteristic } from '../../d/todo';
 
 export abstract class TodoRepository{
-    abstract addTodo(todo:TodoEntity):Promise<TodoEntity>
-    abstract updateTodo(id:string):Promise<TodoEntity>
-    abstract deleteTodo(id:string):Promise<TodoEntity>
+    abstract addTodo(todo:TodoEntity):Promise<void>
+    abstract updateTodo(id:string):Promise<void>
+    abstract deleteTodo(id:string):Promise<void>
     abstract getTodos(): Promise<TodoEntity[]>
     abstract getStrategy( characteristic: TodoCharacteristic ):Promise<TodoEntity>
 }
